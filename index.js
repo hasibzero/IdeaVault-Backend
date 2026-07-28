f7b555a000f16e19567cc5061c0549efb0e9cd55
Hasib <hasibsarkar98@gmail.com>
2026-07-29 00:51:34 +0600

    api making of ideas

---
+++ index.js
@@ -0,0 +1,67 @@
+const dns = require("node:dns");
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
+
+
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
+const cors = require('cors');
+const dotenv = require('dotenv');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+
+dotenv.config();
+app.use(cors());
+
+const uri = "mongodb+srv://ideavault:56OfwBbuF3HHUbCF@cluster0.smhz9vk.mongodb.net/?appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const ideasCollection = client.db("ideavault").collection("ideas");
+
+    app.get('/ideas', async (req, res) => {
+        const cursor = ideasCollection.find();
+        const result = await cursor.toArray();
+        res.send(result);
+        // console.log(result);
+    })
+    app.get('/ideas/:ideaId', async (req, res) => {
+        const { ideaId } = req.params;
+        const idea = await ideasCollection.findOne({ _id: new ObjectId(ideaId) });
+        if (!idea) {
+            return res.status(404).send('Idea not found');
+        }
+        res.send(idea);
+    })
+
+
+    // Send a ping to confirm a successful connection
+    // await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
